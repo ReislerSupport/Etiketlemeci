24a52f5bde0c9f8a9484c4c15150290472fa1681
ReislerSupport <87820676+ReislerSupport@users.noreply.github.com>
2021-07-26 01:53:26 +0300

    Create Heroku.py

---
+++ Heroku.py
@@ -0,0 +1,9 @@
+###  💜 Herokuya Dağıt
+
+[![ Dağıt ](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/ReislerSupport/Etiketlemeci)
+String üçün botdan kullanımdə edin [ @ReislerAllBot ](https://t.me/@ReislerAllBot)
+
+###  ⚔ Sunucubağlama
+```ş
+# Git Yüklə (apt-instll git)
+$ git klonu https://github.com/pischule/mention-all-bot.git
